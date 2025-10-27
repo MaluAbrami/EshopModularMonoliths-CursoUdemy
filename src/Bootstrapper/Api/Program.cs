803d9f4043a26a54996876d5c5e943d61f223acb
Malu Abrami <marialuizaabrami@gmail.com>
2025-10-27 12:28:40 -0300

    feat(program): registrar Carter e configurar injeção de dependências/rotas no catalogModule

--- src/Bootstrapper/Api/Program.cs
+++ src/Bootstrapper/Api/Program.cs
@@ -1,4 +1,14 @@
+using Carter;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddCarter(configurator: config =>
+{
+    var catalogModules = typeof(CatalogModule).Assembly.GetTypes()
+        .Where(t => t.IsAssignableTo(typeof(ICarterModule))).ToArray();
+
+    config.WithModules(catalogModules);
+});
 
 builder.Services
     .AddCatalogModule(builder.Configuration)
@@ -6,6 +16,8 @@ builder.Services
     .AddOrderingModule(builder.Configuration);
 
 var app = builder.Build();
+
+app.MapCarter();
 
 app
     .UseCatalogModule()
